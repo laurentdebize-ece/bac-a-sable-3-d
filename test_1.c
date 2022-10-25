c4fc4c16eb2f9a451e562a9f72e91a5d512b50c9
RENAUD Maxime <maxime;renaud@edu.ece.fr>
2022-10-25 17:02:43 +0200

    test_1.c et test_1.h avec CMakeLists.txt mis a jour

---
+++ test_1.c
@@ -0,0 +1,5 @@
+//
+// Created by RENAUD Maxime on 25/10/2022.
+//
+
+#include "test_1.h"
---
+++ test_1.h
@@ -0,0 +1,8 @@
+//
+// Created by RENAUD Maxime on 25/10/2022.
+//
+
+#ifndef BAC_A_SABLE_2_TEST_1_H
+#define BAC_A_SABLE_2_TEST_1_H
+
+#endif //BAC_A_SABLE_2_TEST_1_H
